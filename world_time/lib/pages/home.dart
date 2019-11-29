3b6ee4d5dfd06a4df2cb7d745d05a73e1d01d9b2
Huzaifa Madani <jafferry@gmail.com>
2019-11-29 18:44:55 +0500

    world time

---
+++ world_time/lib/pages/home.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  Map data = {};
+  @override
+  Widget build(BuildContext context) {
+
+    data = ModalRoute.of(context).settings.arguments;
+    print(data);
+
+    String img = data['isDay'] ? 'day.jpg' : 'night.jpg';
+    Color bgColor = data['isDay'] ? Colors.blue : Colors.grey[900];
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/$img'),
+            fit: BoxFit.cover,
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0.0, 120.0, 0.0, 0.0),
+          child: SafeArea(
+              child: Column(
+                children: <Widget>[
+                  FlatButton.icon(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/location');
+                      } ,
+                      icon: Icon(
+                          Icons.edit_location,
+                          color: Colors.grey[200],
+                      ),
+                      label: Text(
+                        'Choose Location',
+                        style: TextStyle(
+                          color: Colors.grey[200],
+                        ),
+                      ),
+                  ),
+                  SizedBox(height: 20.0,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                      ),)
+                    ],
+                  ),
+                  SizedBox(height: 20.0,),
+                  Text(
+                    data['time'],
+                    style: TextStyle(
+                      fontSize: 66.0,
+                    ),
+                  )
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
